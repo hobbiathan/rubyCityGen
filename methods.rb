b5509cd0a766bdcf671f77118f752def3db9bd3c
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-18 14:25:32 -0600

    Update generate_cities(), add list_cities()

--- methods.rb
+++ methods.rb
@@ -7,13 +7,23 @@ def generate_cities()
 
   puts "Enter the number of cities you'd like to generate: "
   print "> "
-  n = gets.chomp.to_i
+  n = gets.chomp.to_i # Make sure our input is an integer-type
 
-  until x > n
-    $cities << City.new(x)
+  until x > n # Execute until our code has met the iterations inputted
+    $cities << City.new(x) # Write to global array
     x += 1
   end
 
   print "\n\n"
   puts "Cities generated."
+end
+
+def list_cities()
+  x = 0
+  while x < $cities.size()
+    $cities[x].city_info
+    print "\n"
+    x += 1
+  end
+
 end
