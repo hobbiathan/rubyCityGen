c5f904034f970a41d5cc891fee92e6e570529e53
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-18 15:15:22 -0600

    Add comments

--- methods.rb
+++ methods.rb
@@ -2,10 +2,12 @@ require './class'
 
 $cities = [] # Global array
 
+### ### ### ### ###
+
 def generate_cities()
   x = 1
 
-  puts "Enter the number of cities you'd like to generate: "
+  puts "\nEnter the number of cities you'd like to generate: "
   print "> "
   n = gets.chomp.to_i # Make sure our input is an integer-type
 
@@ -18,12 +20,17 @@ def generate_cities()
   puts "Cities generated."
 end
 
-def list_cities()
-  x = 0
-  while x < $cities.size() # Loop through all elements in our global array
-    $cities[x].city_info # Print out info of each element 
-    print "\n"
-    x += 1
-  end
+### ### ### ### ###
 
+def list_cities()
+  if $cities.size() == 0
+    puts "\nThere are no cities.\n"
+  else
+    x = 0
+    while x < $cities.size() # Loop through all elements in our global array
+      $cities[x].city_info # Print out info of each element
+      print "\n"
+      x += 1
+    end
+  end
 end
