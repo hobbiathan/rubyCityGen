31897f86d386d9ad585f20e0e58e041b87e71166
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-18 14:29:07 -0600

    Add basic control-flow for user-input

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 # Keeping our class definition and structure in a separate file
 require './class'
+require './methods'
+$running = 1
+# The multi-line comment below was testing for object creation and insertion into an array
+=begin
 
 # Creating an array empty array
 cities = []
@@ -13,4 +17,24 @@ until x >= cities.size() # Execute this code until conditional is met
   cities[x].city_info # Print off info of each element in our array
   print "\n" #
   x += 1
+end
+
+=end
+
+# Begin control flow
+while $running == 1
+  puts "What would you like to do?"
+  puts "Generate Cities (G), List Cities (L)"
+  input = gets.chomp
+
+  if input == 'G'
+    generate_cities()
+    $running = 1
+  elsif input == 'L'
+    list_cities()
+    $running = 1
+  else
+    puts "Invalid input, program terminated."
+    $running = 0
+  end
 end
